2f5616ef8c1f76fae04a59ecc1b837788c6930eb
Maxim <m.noltmeer@gmail.com>
2021-12-18 20:34:38 +0200

    added copyright data

--- Logs.cpp
+++ Logs.cpp
@@ -1,4 +1,22 @@
+/*!
+Copyright 2018-2021 Maxim Noltmeer (m.noltmeer@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //---------------------------------------------------------------------------
+
 #include <memory>
 #pragma hdrstop
 
@@ -26,7 +44,7 @@ void AddToFile(String file, String text)
 
 	  ms->Position = 0;
 	  srv_file->Position = srv_file->Size;
-	  srv_file->CopyFrom(ms, ms->Size);
+	  srv_file->CopyFrom(ms.get(), ms->Size);
 	}
 }
 //---------------------------------------------------------------------------
